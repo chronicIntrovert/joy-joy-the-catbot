427b375ed3fa2a1202afb71da9aaa4def78d411a
Thinh Vu <thinh.vu@ge.com>
2017-01-24 23:02:56 -0800

    Testing out FacebookMessengerBot framework.

--- index.js
+++ index.js
@@ -1,94 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request-promise');
+import { Bot, Elements } from 'facebook-messenger-bot';
+
+const bot = new Bot(process.env.PAGE_ACCESS_TOKEN, process.env.VERIFY_TOKEN);
+
+bot.on('message', async message => {
+    const { sender } = message;
+    await sender.fetch('first_name');
+
+    const out = new Elements();
+    out.add({ text: `hey ${sender.first_name}, how are you!` });
+
+    await bot.send(sender.id, out);
+});
+
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-const server = app.listen(process.env.PORT || 5000, () => {
-    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
-
-/* For Facebook Validation */
-app.get('/webhook', (req, res) => {
-    if (req.query['hub.mode'] && req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
-        res.status(200).send(req.query['hub.challenge']);
-    } else {
-        res.status(403).end();
-    }
-});
-
-/* Handling all messages */
-app.post('/webhook', (req, res) => {
-    console.log(req.body);
-    if (req.body.object === 'page') {
-        req.body.entry.forEach((entry) => {
-            entry.messaging.forEach((event) => {
-                if (event.message && event.message.text) {
-                    sendMessage(event);
-                    sendCat(event);
-                }
-            });
-        });
-        res.status(200).end();
-    }
-});
-
-function sendMessage(event) {
-    let sender = event.sender.id;
-    let text = event.message.text;
-
-    request({
-        uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-        method: 'POST',
-        json: {
-            recipient: { id: sender },
-            message: { text: "Joy Joy went off to fetch you one of her friends, please wait as she scurries off for a bit..." }
-        }
-    }, function(error, response) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-}
-
-function sendCat(event) {
-    let sender = event.sender.id;
-
-    request({
-        uri: 'http://api.giphy.com/v1/gifs/random',
-        qs: {
-            api_key: 'dc6zaTOxFJmzC',
-            tag: 'cat'
-        },
-        json: true
-    }).then((data) => {
-        request({
-            uri: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-            method: 'POST',
-            json: {
-                "recipient": { "id": sender },
-                "message": {
-                    "attachment": {
-                        "type": "image",
-                        "payload": {
-                            "url": data.data.image_url
-                        }
-                    }
-                }
-            }
-        }, function(error, response) {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        });
-    }).catch((err) => {
-        console.log(err);
-    });
-}
+app.use('/webhook', bot.router());
+app.listen(3000);
