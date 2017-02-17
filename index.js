7858bb18838d7976699587f2b43f36664b124dbf
Thinh Vu <thinh.vu@ge.com>
2017-02-17 00:09:21 -0800

    Added yarn.lock file & refactored code.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request-promise');
+const request = require('requestretry');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
-/* For Facebook Validation */
 app.get('/webhook', (req, res) => {
     if (req.query['hub.mode'] && req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
         res.status(200).send(req.query['hub.challenge']);
@@ -38,7 +37,7 @@ function sendMessage(event) {
     let sender = event.sender.id;
     let text = event.message.text;
 
-    request({
+    const options = {
         uri: 'https://graph.facebook.com/v2.6/me/messages',
         qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
         method: 'POST',
@@ -46,49 +45,55 @@ function sendMessage(event) {
             recipient: { id: sender },
             message: { text: "Joy Joy went off to fetch you one of her friends, please wait as she scurries off for a bit..." }
         }
-    }, function(error, response) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
+    };
+
+    request(options).then(response => {
+        console.log('Error: ', response.body.error);
+    }).catch(error => {
+        console.log('Error sending message: ', error);
+    });
+}
+
+function retrieveCat(data) {
+    const options = {
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: {
+            "recipient": { "id": sender },
+            "message": {
+                "attachment": {
+                    "type": "image",
+                    "payload": {
+                        "url": data.data.image_url
+                    }
+                }
+            }
         }
+    };
+
+    request(options).then(response => {
+        console.log('Error: ', response.body.error);
+    }).catch(error => {
+        console.log('Error sending message: ', error);
     });
 }
 
 function sendCat(event) {
     let sender = event.sender.id;
 
-    request({
+    const options = {
         uri: 'http://api.giphy.com/v1/gifs/random',
         qs: {
             api_key: 'dc6zaTOxFJmzC',
             tag: 'cat'
         },
         json: true
-    }).then((data) => {
-        request({
-            uri: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-            method: 'POST',
-            json: {
-                "recipient": { "id": sender },
-                "message": {
-                    "attachment": {
-                        "type": "image",
-                        "payload": {
-                            "url": data.data.image_url
-                        }
-                    }
-                }
-            }
-        }, function(error, response) {
-            if (error) {
-                console.log('Error sending message: ', error);
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error);
-            }
-        });
-    }).catch((err) => {
-        console.log(err);
+    };
+
+    request(options).then(data => {
+        retrieveCat(data);
+    }).catch((error) => {
+        console.log('Error retrieveing cat GIF from Giphy: ', error);
     });
 }
